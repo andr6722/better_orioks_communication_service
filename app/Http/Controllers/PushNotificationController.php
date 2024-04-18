5ebb697eb60a688d38f7315c05a006094fd619dc
manok <andrey6722@gmail.com>
2024-04-18 23:01:22 +0300

    Add log

--- app/Http/Controllers/PushNotificationController.php
+++ app/Http/Controllers/PushNotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
 use Kreait\Laravel\Firebase\Facades\Firebase;
@@ -10,12 +11,32 @@ use RuntimeException;
 
 class PushNotificationController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/v1/notifications",
+     *     tags={"notifications"},
+     *     summary="Send a push notifications",
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="User data",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="user_id", type="string", example="123"),
+     *              @OA\Property(property="subject_name", type="string", example="abcd1234"),
+     *              @OA\Property(property="control_event_name", type="string", example="sadf3423"),
+     *              @OA\Property(property="current_score", type="string", example="_"),
+     *              @OA\Property(property="new_score", type="string", example="5")
+     *          )
+     *      ),
+     *     @OA\Response(response="302", description="Displays a push notifications about rating changes")
+     * )
+     */
     public function sendNotification(Request $request): \Illuminate\Http\JsonResponse
     {
 
         $json = $request -> json()->all();
         $token = $json['user_id'];
         $body = "Предмет: {$json['subject_name']}, КМ {$json['control_event_name']}. Оценка изменена с {$json['current_score']}, на {$json['new_score']}";
+        Log::channel('notificationlog') -> debug($body);
         try{
             $messaging = Firebase::messaging();
             $notification_push = Notification::fromArray([
@@ -31,9 +52,27 @@ class PushNotificationController extends Controller
             return response()->json($body);
         }
     }
+    /**
+     * @OA\Post(
+     *     path="/api/v1/news",
+     *     tags={"News"},
+     *     summary="Send a push notifications",
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="User data",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="user_id", type="string", example="123"),
+     *              @OA\Property(property="NewsName", type="string", example="abcd1234"),
+     *              @OA\Property(property="link", type="string", example="sadf3423")
+     *          )
+     *      ),
+     *     @OA\Response(response="302", description="Displays a push notifications about rating changes")
+     * )
+     */
     public function sendNews(Request $request): \Illuminate\Http\JsonResponse{
         $json = $request -> json()->all();
         $token = $json['user_id'];
+        Log::channel('notificationlog') -> debug($json);
         try{
             $messaging = Firebase::messaging();
             $title = 'News';
